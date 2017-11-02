e599b4bb46bb0abe0c523e296692da44f058126c
Artefall <32461472+Artefall@users.noreply.github.com>
2017-11-02 18:44:29 +0200

    Added function

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-$(window).on('deviceorientation', function(jqevent){
+var alphaCountMain = $(window).on('deviceorientation', function(jqevent){
     var event = jqevent.originalEvent;
-    console.log(event.alpha);
+    var alphaCount = (event.alpha);
+    return alphaCount;
 })
 
+function findNaD(degrees){
+    switch(degrees){
+        case (degrees >= 338 && degrees <= 22):
+            return N;
+        case (degrees >= 23 && degrees <= 67):
+            return NE;
+        case (degrees >= 68 && degrees <= 112):
+            return E;
+        case (degrees >= 113 && degrees <= 157):
+            return ES;
+        case (degrees >= 158 && degrees <= 202):
+            return S;
+        case (degrees >= 203 && degrees <= 247):
+            return SW;
+        case (degrees >= 248 && degrees <= 293):
+            return W;
+        case (degrees >= 294 && degrees <= 337):
+            return WN;
+           
+    }
 
+}
