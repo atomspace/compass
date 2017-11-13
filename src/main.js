3652a5a2a477c169a4cbd8ff3efd3ae482dd1e69
Artefall <32461472+Artefall@users.noreply.github.com>
2017-11-13 20:22:00 +0200

    Added eslint

--- src/main.js
+++ src/main.js
@@ -36,7 +36,7 @@
 //             return W;
 //         case (degrees >= 294 && degrees <= 337):
 //             return WN;
-           
+
 //     }
 
 // }
@@ -48,65 +48,64 @@
 // $(".direction").html(readyName);
 
 // $(window).on('deviceorientation', handleDeviceOrientation)
-$(window).on('deviceorientation', function(jqevent){
+$(window).on('deviceorientation', function (jqevent) {
     var event = jqevent.originalEvent;
     var alphaCount = (event.alpha);
 
-    var realDeg = converter(alphaCount)
-    
-    var realName = findName(realDeg)
+    var realDeg = converter(alphaCount);
+
+    var realName = findName(realDeg);
 
 
     var spinPart = $('.spinPart');
-    spinPart.css('transform','rotate('+alphaCount+'deg)');
- 
-    var direction = $('.direction')
-    direction.html(realName)
+    spinPart.css('transform', 'rotate(' + alphaCount + 'deg)');
+
+    var direction = $('.direction');
+    direction.html(realName);
 
     var degreeValue = $('.degreeValue');
-    degreeValue.html( realDeg + '°');
+    degreeValue.html(realDeg + '°');
 
 
     // console.log(alphaCount);
     // console.log(realName);
     // console.log(realDeg);
-})
+});
 
-function converter(deg){
-    if (deg <= 0 && deg > -360){
+function converter(deg) {
+    if (deg <= 0 && deg > -360) {
         return Math.abs(Math.round(deg));
     }
-    else if (deg > 0 && deg < 360 ){
+    else if (deg > 0 && deg < 360) {
         return (360 - Math.round(deg));
     }
 
 
 }
 
-function findName(degrees){
-    if (degrees >= 338 && degrees <= 360 || degrees <= 22 && degrees >= 0 || degrees >= -338 && degrees <= -360 || degrees <= -22 && degrees >= 0)
-        return "N";            
-    else if (degrees >= 23 && degrees <= 67 || degrees >= -23 && degrees <= -67){
-        return "NE";
+function findName(degrees) {
+    if (degrees >= 338 && degrees <= 360 || degrees <= 22 && degrees >= 0 || degrees >= -338 && degrees <= -360 || degrees <= -22 && degrees >= 0) { return 'N'; }
+    else if (degrees >= 23 && degrees <= 67 || degrees >= -23 && degrees <= -67) {
+        return 'NE';
     }
-    else if (degrees >= 68 && degrees <= 112 || degrees >= -68 && degrees <= -112){
-        return "E";
+    else if (degrees >= 68 && degrees <= 112 || degrees >= -68 && degrees <= -112) {
+        return 'E';
     }
-    else if (degrees >= 113 && degrees <= 157 || degrees >= -113 && degrees <= -157){
-        return "ES";
+    else if (degrees >= 113 && degrees <= 157 || degrees >= -113 && degrees <= -157) {
+        return 'ES';
     }
-    else if (degrees >= 158 && degrees <= 202 || degrees >= -158 && degrees <= -202){
-        return "S";
+    else if (degrees >= 158 && degrees <= 202 || degrees >= -158 && degrees <= -202) {
+        return 'S';
     }
-    else if (degrees >= 203 && degrees <= 247 || degrees >= -203 && degrees <= -247){
-        return "SW";
+    else if (degrees >= 203 && degrees <= 247 || degrees >= -203 && degrees <= -247) {
+        return 'SW';
     }
-    else if (degrees >= 248 && degrees <= 293 || degrees >= -248 && degrees <= -293){
-        return "W";
+    else if (degrees >= 248 && degrees <= 293 || degrees >= -248 && degrees <= -293) {
+        return 'W';
     }
-    else if (degrees >= 294 && degrees <= 337 || degrees >= -294 && degrees <= -337){
-        return "WN";
-    }   
-    
+    else if (degrees >= 294 && degrees <= 337 || degrees >= -294 && degrees <= -337) {
+        return 'WN';
+    }
+
 
 }
